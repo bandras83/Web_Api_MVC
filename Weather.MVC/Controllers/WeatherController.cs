5b1e09a772ea577a52d7507a2517dc1c933f41b0
andri <andri@ANDRIS-E580>
2024-05-06 21:18:05 +0200

    Add project files.

---
+++ Weather.MVC/Controllers/WeatherController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using Weather.MVC.Models;
+
+namespace Weather.MVC.Controllers
+{
+	public class WeatherController : Controller
+	{
+		private Uri _baseAddress = new Uri("https://localhost:7183");
+		private HttpClient _httpClient;
+
+        public WeatherController()
+        {
+            _httpClient = new HttpClient();
+			_httpClient.BaseAddress = _baseAddress;
+        }
+
+        [HttpGet]
+		public IActionResult Index()
+		{
+			var weatherList = new List<WeatherViewModel>();
+			var response = _httpClient.GetAsync(_httpClient.BaseAddress + "WeatherForecast/Get").Result;
+			if (response.IsSuccessStatusCode)
+			{
+				var responseString = response.Content.ReadAsStringAsync().Result;
+				weatherList = JsonConvert.DeserializeObject<List<WeatherViewModel>>(responseString);
+			}
+
+			return View(weatherList);
+		}
+	}
+}
---
+++ Weather.Test/ModelValidator_Test.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json.Schema.Generation;
+using NUnit.Framework;
+using Weather.Model;
+using Weather.MVC.Models;
+
+namespace Weather.Test
+{
+	[TestFixture]
+	public class ModelValidator_Test
+	{
+		[Test]
+		public void ModelsAreCompatible()
+		{
+			var generator = new JSchemaGenerator();
+			var modelSchema = generator.Generate(typeof(WeatherForecast));
+			var viewModelSchema = generator.Generate(typeof(WeatherViewModel));
+			var s1 = modelSchema.ToString();
+			var s2 = viewModelSchema.ToString();
+			s1.Should().Be(s2);
+		}
+	}
+}
